21b049fb320ca07ec385fc379ac63fd9b1d0e3ac
mjaoanan <137941785+mjaoanan@users.noreply.github.com>
2023-08-08 21:01:45 -0700

    fix

--- chart2_example.R
+++ chart2_example.R
@@ -1,6 +1,20 @@
-x_values <- seq(1, 3)
-y_values <- seq(1,3)
-
+library(maps)
+library(ggmap)
 library(ggplot2)
-ggplot() +
-  geom_line(aes(x=x_values, y = y_values))
+library(tidyverse)
+library(dplyr)
+library(tidyr)
+
+prison_data <- read.csv("https://raw.githubusercontent.com/melaniewalsh/Neat-Datasets/main/us-prison-jail-rates-1990-WA.csv")
+
+
+## GRAPH 2
+
+race_Comparison <- prison_data %>%
+  filter(complete.cases(black_prison_pop_rate), complete.cases(white_prison_pop_rate)) %>%
+  select (year, black_prison_pop_rate, white_prison_pop_rate) %>%
+  gather (key = Race, value = population, -year)
+
+comparisonGraph <- ggplot(race_Comparison) +
+  geom_col(mapping = aes(x = year, y = population, fill = Race))
+comparisonGraph
